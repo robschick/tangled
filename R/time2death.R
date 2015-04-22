bea168b6f045719564e2c868f9a114e7b842e713
= <=>
2015-04-22 12:16:59 +0100

    This is to make density strip plots of time until death

---
+++ R/time2death.R
@@ -0,0 +1,92 @@
+# Coding by Rob Schick, April 20, 2015
+# The goal of this is to make the box plots of time to death by entanglement severity
+
+rm(list=ls())
+library(ggplot2)
+library(lubridate)
+library(reshape)
+library(scales)
+library(grid)
+library(gridExtra)
+library(RColorBrewer)
+library(stringr)
+library(plyr)
+source('/Users/rob/Documents/code/rss10/rightwhales/makeYearmon.r')
+wd <- '/Users/rob/Documents/research/projects/PCAD/rightwhales/'
+setwd(wd)
+load(file = paste(wd, 'gibbsoutput/eg_105_ng_50000_BIG_50000.rdata', sep = '') )
+load(file="data/egAmyEntData.rdata")
+tSub <- tangleOut
+
+events <- numeric(0)
+for(egno in unique(tSub$EGNo)){
+  dsub <- subset(tSub, EGNo == egno)
+  dsub <- dsub[which.max(dsub$EventNo),]
+  events <- rbind(events, dsub)
+}
+
+# need to find the max estimate for death from deathyr
+# this will produce 'dtime' which is that month of imputed death
+idx <- events$EGNo %in% ID
+events <- events[idx, ]
+events$dtime <- NA
+events$knownD <- FALSE
+for(i in 1:length(unique(events$EGNo))){
+  id <- which(ID == unique(events$EGNo)[i])
+  dsub <- deathyr[id, ]
+  events$dtime[i] <- which.max(dsub)
+  if(any(dsub == 50000) & events$dtime[i] < nt){
+    events$knownD[i] <- TRUE
+  }
+}
+
+# table(events$knownD)
+
+
+df <- events # as compared to dfsub, this includes all animals 
+df$dtime <- ifelse(df$dtime > nt, nt, df$dtime)
+df <- df[df$knownD == FALSE,]
+df$emonyrID <- match(df$emonyr, myName)
+df$ldwgmonyrID <- match(df$ldwgmonyr, myName)
+df$m2die <- ifelse(!is.na(df$ldwgmonyrID), df$dtime - df$ldwgmonyrID, df$dtime - df$emonyrID)
+
+dfsub <- events[events$dtime < nt,] # as compared to df, this includes all animals presumed dead before nt
+dfsub <- dfsub[dfsub$knownD == FALSE,]
+dfsub$emonyrID <- match(dfsub$emonyr, myName)
+dfsub$ldwgmonyrID <- match(dfsub$ldwgmonyr, myName)
+dfsub$m2die <- ifelse(!is.na(dfsub$ldwgmonyrID), dfsub$dtime - dfsub$ldwgmonyrID, dfsub$dtime - dfsub$emonyrID)
+
+
+# # Plot out the distributions
+pdf(file = '/Users/rob/Dropbox/Talks/2015/SEIT_RightWhales/time2die.pdf', width = 9.4, height = 5.9)
+  x <- seq(-4, 4, length=10000)
+par(xpd = FALSE)
+  plot(x, xlim=c(0, 250), ylim=c(1, 6), xlab='Time until Death (Years)', ylab="", type="n", las = 1, bty = 'n', axes = FALSE)
+  axis(side = 2, at = seq(6), 
+       labels = c('Severe \n Gear', 'Mod. \n Gear', 'Severe \n No Gear', 
+                  'Min. \n Gear', 'Mod. \n No Gear', 'Min. \n No Gear'), las = 1, cex.axis = .75)
+  axis(side = 1, at = seq(0, 20 * 12, by = 12), labels = seq(0, 20 * 12, by = 12) / 12, cex.axis = .65) # x axis
+  abline(v = seq(12, 20 * 12, by = 12), col = 'grey80')
+  abline(v = seq(60, 20 * 12, by = 60), col = 'grey40')
+  denstrip(x = dfsub$m2die[dfsub$gearInj == 1], at = 1, ticks = median(dfsub$m2die[dfsub$gearInj == 1]), twd = 3)
+  denstrip(x = dfsub$m2die[dfsub$gearInj == 2], at = 2, ticks = median(dfsub$m2die[dfsub$gearInj == 2]), twd = 3)
+  denstrip(x = dfsub$m2die[dfsub$gearInj == 3], at = 3, ticks = median(dfsub$m2die[dfsub$gearInj == 3]), twd = 3)
+  denstrip(x = dfsub$m2die[dfsub$gearInj == 5], at = 5, ticks = median(dfsub$m2die[dfsub$gearInj == 5]), twd = 3)
+  denstrip(x = dfsub$m2die[dfsub$gearInj == 6], at = 6, ticks = median(dfsub$m2die[dfsub$gearInj == 6]), twd = 3)
+dev.off()
+
+pdf(file = '/Users/rob/Dropbox/Talks/2015/SEIT_RightWhales/time2dieAll.pdf', width = 9.4, height = 5.9)
+plot(x, xlim=c(0, 250), ylim=c(1, 6), xlab='Time (Years)', ylab="", type="n", las = 1, bty = 'n', axes = FALSE)
+axis(side = 2, at = seq(6), 
+     labels = c('Severe \n Gear', 'Mod. \n Gear', 'Severe \n No Gear', 
+                'Min. \n Gear', 'Mod. \n No Gear', 'Min. \n No Gear'), las = 1, cex.axis = .75)
+axis(side = 1, at = seq(0, 20 * 12, by = 12), labels = seq(0, 20 * 12, by = 12) / 12, cex.axis = .65) # x axis
+abline(v = seq(12, 20 * 12, by = 12), col = 'grey80')
+abline(v = seq(60, 20 * 12, by = 60), col = 'grey40')
+  denstrip(x = df$m2die[df$gearInj == 1], at = 1, ticks = median(df$m2die[df$gearInj == 1]), twd = 3)
+  denstrip(x = df$m2die[df$gearInj == 2], at = 2, ticks = median(df$m2die[df$gearInj == 2]), twd = 3)
+  denstrip(x = df$m2die[df$gearInj == 3], at = 3, ticks = median(df$m2die[df$gearInj == 3]), twd = 3)
+  denstrip(x = df$m2die[df$gearInj == 3], at = 4, ticks = median(df$m2die[df$gearInj == 4]), twd = 3)
+  denstrip(x = df$m2die[df$gearInj == 5], at = 5, ticks = median(df$m2die[df$gearInj == 5]), twd = 3)
+  denstrip(x = df$m2die[df$gearInj == 6], at = 6, ticks = median(df$m2die[df$gearInj == 6]), twd = 3)
+dev.off()
