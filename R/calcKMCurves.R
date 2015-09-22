15f822c4e603df83ce3690c9aed533d988abf703
= <=>
2015-09-22 11:49:18 +0100

    Adding in the code to calculate and plot individual curves, i.e. by severity

--- R/calcKMCurves.R
+++ R/calcKMCurves.R
@@ -11,6 +11,8 @@ survdf$censYearInt <- survdf$deathyearInt <- findInterval(survdf$deathMonth0, se
 # for testing
 # idx <- sample(unique(survdf$EGNo), 20)
 # dsub <- survdf[survdf$EGNo %in% idx, ]
+
+# All entanglement events
 dsub <- survdf
 # dtimes <- unique(dsub$deathMonth0) # when the animals died or are censored
 
@@ -55,16 +57,63 @@ ggplot(kmdf, aes(interval, psurv)) +
   scale_x_continuous(breaks = seq(0, 30, 5), minor_breaks = seq(0, 30, 1))
 dev.off()
 
+# Break it out by entanglement severity:
+kmdfAll <- numeric(0)
+csubAll <- numeric(0)
+for(j in 1:3){
+  svvec <- c("moderate", "minor", "severe" )
+  dsub <- survdf[survdf$Severity == svvec[j], ]
+  kmdf <- data.frame(interval = 0, atRiskStart = nrow(dsub), censored = 0,
+                     diedOnInterval = 0, atRiskEnd = 0, propSurv = 1)
+  for(i in 1:length(unique(dsub$yearInt))){
+    dsubi <- dsub[which(dsub$yearInt == i), ]
+    interval <- i
+    
+    atRiskStart <- length(unique(dsubi$EGNo))
+    censored <- length(which(!duplicated(dsubi$EGNo) & dsubi$censored == TRUE & dsubi$deathyearInt == interval))
+    died <- length(which(!duplicated(dsubi$EGNo) & dsubi$censored == FALSE & dsubi$deathyearInt == interval))
+    atRiskEnd <- atRiskStart - (censored + died)
+    proportionSurviving <- atRiskEnd / atRiskStart
+    
+    kmdf <- rbind(kmdf, data.frame(interval = interval, atRiskStart = atRiskStart, censored = censored,
+                                   diedOnInterval = died, atRiskEnd = atRiskEnd, propSurv = proportionSurviving))
+  }
+  kmdf$psurv <- cumprod(kmdf$propSurv)
+  kmdf$sev <- svvec[j]
+  kmdfAll <- rbind(kmdfAll, kmdf)
+  
+  # I want a censored month in here as well for plotting purposes
+  csub <- subset(dsub, censored == TRUE)
+  csub <- csub[!duplicated(csub$EGNo),]
+  psvec <- rep(NA, nrow(csub))# to store the death months
+  
+  for(i in 1:nrow(csub)){
+    interval <- csub[i, 'censYearInt']
+    psvec[i] <- kmdf[kmdf$interval == (interval - 1), 'psurv']
+  }
+  
+  csub$psurv <- psvec
+  csub$sev <- svvec[j]
+  
+  csubAll <- rbind(csubAll, csub)
+  
+} # end loop over severity
+
+
+
 
 pdf(file = '/Users/rob/Dropbox/Papers/KnowltonEtAl_Entanglement/images/survivalCurveBySeverity.pdf', width = 8, height = 6)
-ggplot(kmdf, aes(interval, psurv)) + 
-  geom_step() + 
+ggplot(kmdfAll, aes(interval, psurv, group = sev, colour = sev)) + 
+  geom_step(lwd = 1) + 
   ylim(0, 1) + 
-  geom_segment(data = csub, aes(x = deathMonth0 / 12, y = psurv, xend = deathMonth0 / 12, yend = psurv + 0.015)) + 
+  geom_segment(data = csubAll, aes(x = deathMonth0 / 12, y = psurv, xend = deathMonth0 / 12, yend = psurv + 0.015)) + 
   labs(y = 'Probability of Survival', x = 'Years Following End of Entanglement')+
   theme_bw()+
-  theme(panel.grid.major = element_line(size = 1.5), panel.grid.minor = element_line(size = 1))+
-  scale_x_continuous(breaks = seq(0, 30, 5), minor_breaks = seq(0, 30, 1))
+  theme(panel.grid.major = element_line(size = 1.25), panel.grid.minor = element_line(size = 1))+
+  scale_x_continuous(breaks = seq(0, 30, 5), minor_breaks = seq(0, 30, 1), expand = c(0, 0))+
+  scale_y_continuous(expand = c(0, 0))+
+  scale_colour_brewer(palette = 'Set2', name = 'Entanglement\nSeverity',
+                      labels = c('Minor', 'Moderate', 'Severe'))
 dev.off()
 
 
