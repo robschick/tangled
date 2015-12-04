250ea9ee1cc3fdafb113b605449407f25c43bf88
= <=>
2015-12-04 13:45:29 +0000

    Functional Check-in of script to make side-by-side boxplots of health

--- R/boxplotHealth.R
+++ R/boxplotHealth.R
@@ -12,29 +12,28 @@ library(plyr)
 source('/Users/rob/Documents/code/rss10/rightwhales/makeYearmon.r')
 load(file = 'data/eg_2015_newData_JUVTRUE__50000_wkspc.rdata')
 load(file="data/egAmyEntData.rdata") 
-load(file = 'data/unimpacted.rdata')
+load(file = 'data/unimpacted.rdata') # contains "nonrepvec" and "repvec"  
 # egAmyEntData.rdata contains tangleOut, tangRepro, tangNonRepro, so use the repro flag
-repro <- TRUE
+for(z in 1:2){
+  ifelse(z == 1, repro <- TRUE, repro <- FALSE)
+
+
+
 if (repro) {
   tSub <- tangRepro 
 } else {
   tSub <- tangNonRepro
 }  
 
-# Update to put an identifier column and then bind both together
-tangRepro$status <- 'RepFem'
-tangNonRepro$status <- 'NonRepFem'
-tSub <- rbind(tangRepro, tangNonRepro)
 
-  
 healthmean <- sumh / g
 for(i in 1:nrow(healthmean)){
   healthmean[i, 1:firstSight[i]] <- NA
 }
 
-dfSum <- matrix(NA, nrow = length(unique(tSub$EGNo)), ncol = 9)
+dfSum <- matrix(NA, nrow = length(unique(tSub$EGNo)), ncol = 10)
 colnames(dfSum) <- c('ID', 'Severe - 1', 'Severe - 0', 'Moderate - 1', 
-                     'Moderate - 0', 'Minor - 1', 'Minor - 0', 'None', 'Post-Reprod')
+                     'Moderate - 0', 'Minor - 1', 'Minor - 0', 'None', 'Post-Reprod', 'repStatus')
 idFac <- factor(unique(tSub$EGNo))
 
 for(i in 1:length(unique(tSub$EGNo))){
@@ -80,6 +79,7 @@ for(i in 1:length(unique(tSub$EGNo))){
   # then extract the health values for these categories
   tSum <- tapply(htest, tfac, median, na.rm = T)  
   dfSum[i, 1] <- ind
+  # dfSum[i, 'repStatus'] <- ind
   
   if ('1' %in% names(tSum) ) {# severe with gear
     dfSum[i, 2] <- tSum[which(names(tSum) == '1')]  
@@ -123,16 +123,24 @@ dfLong[mod1idx, 'gearnogear'] <- 1
 dfLong[min1idx, 'gearnogear'] <- 1
 
 # add in the unimpacted animals - using data coming from the splitpopHealth.R function
+# ONly adding them in once to avoid the double data, i.e. adding the same unimpacted data for both
+# the rep and the non-rep, when we really only want them once.
 if (repro) {
   uvec <- repvec
-} else {
+  dfuvec <- data.frame(ID = 9999,
+                       variable = 'unimpacted',
+                       value = uvec,
+                       gearnogear = 0)
+  dfLong <- rbind(dfLong, dfuvec)
+}  else {
   uvec <- nonrepvec
-}  
-dfuvec <- data.frame(ID = 9999,
-                     variable = 'unimpacted',
-                     value = uvec,
-                     gearnogear = 0)
-dfLong <- rbind(dfLong, dfuvec)
+  dfurvec <- data.frame(ID = 9999,
+                       variable = 'unimpacted',
+                       value = uvec,
+                       gearnogear = 0)
+  dfLong <- rbind(dfLong, dfurvec)
+}
+
 
 dfLong$group <- 1
 mod0idx <- which(dfLong$variable == 'Moderate...0')
@@ -142,7 +150,21 @@ dfLong[c(mod1idx, mod0idx), 'group'] <- 2
 dfLong[c(min1idx, min0idx), 'group'] <- 3
 dfLong[unimpactedidx, 'group'] <- 4
 dfLong$group <- factor(dfLong$group, levels = c(4, 3, 2, 1) )
-save(dfLong, file = 'data/healthEntanglementWindowData.rdata')
+
+# # Update to put an identifier column and then bind both together
+if (repro) {
+  dfLongRepro <- dfLong
+  dfLongRepro$status <- 'RepFem'
+  } else {
+  dfLongNonRepro <- dfLong
+  dfLongNonRepro$status <- 'NonRepFem'
+}  
+
+
+
+
+
+
 # run the glm 
 # first relevel so unimpacted is the reference level.
 dfglm <- transform(dfLong, variable = factor(variable))
@@ -152,9 +174,10 @@ ft1 <- glm(value ~ variable, data = dfglm)
 summary(ft1)
 summary(glht(ft1, mcp(variable = "Tukey")))
 
-# set up for labeling the box plots
-nvals <- as.vector(table(tSub$gearInj))
-plabel <- c(nvals[6], nvals[4], nvals[5], nvals[2], nvals[3], nvals[1])
+} # end loop over the 'repro' variable
+save(dfLongRepro, dfLongNonRepro, file = 'data/healthEntanglementWindowDataBoth.rdata') # Both means it's containing repro and non-repro fems
+
+
 
 # Commenting this all out because I'm now going to do both side-by-side
 # if (repro) {
@@ -184,19 +207,25 @@ plabel <- c(nvals[6], nvals[4], nvals[5], nvals[2], nvals[3], nvals[1])
 #   print(p)
 # dev.off()
 
+# set up for labeling the box plots
+dfLong <- rbind(dfLongRepro, dfLongNonRepro)
+nvals <- as.vector(table(dfLong$status, dfLong$variable))
+plabel <- c(nvals[11], nvals[12], nvals[9], nvals[10], nvals[7], nvals[8],
+            nvals[5], nvals[6], nvals[3], nvals[4], nvals[1], nvals[2])
 
-
-name <- '/Users/robs/Dropbox/Papers/KnowltonEtAl_Entanglement/images/BoxPlotHealthByEntglClass_NonReproAndReproAnimals.pdf'
-# This is per Amy's request to include both categories side by side. Instead of breaking down the gear/no-gear for shading
+name <- '/Users/rob/Dropbox/Papers/KnowltonEtAl_Entanglement/images/BoxPlotHealthByEntglClass_NonReproAndReproAnimals.pdf'
+# This updated plot is per Amy's request to include both categories side by side. Instead of breaking down the gear/no-gear for shading
 # the shading will be repro vs non-repro
-p <- ggplot(dfLong, aes(x = factor(group), y = value)) +
-  geom_boxplot(aes(fill = factor(gearnogear), group = paste(factor(variable), group)), outlier.shape=NA, notch = FALSE)+
-  annotate('text', x = c(1 + 0.82, 1 + 1.18, 1 + 1.82, 1 + 2.18, 1 + 2.82, 1 + 3.18), y = 92.5, 
-           label = plabel, cex = 3)+
-  labs(y = 'Estimated Health', x = 'Injury Status', fill = 'Gear Status')+
-  scale_x_discrete(labels = c('Unimpacted', 'Minor', 'Moderate', 'Severe'))+
-  scale_fill_grey(start = 1, end = 0.65,labels = c('No Gear', 'Carrying Gear'))+
-  theme_bw()
+p <- ggplot(dfLong, aes(x = factor(variable, levels = c('unimpacted', 'Minor...0', 'Minor...1', 'Moderate...0', 'Moderate...1', 'Severe...0', 'Severe...1')),y = value)) +
+  geom_boxplot(aes(fill = factor(status), group = paste(factor(variable), status)), outlier.shape=NA, notch = FALSE)+
+  annotate('text', x = c(1 + 0.82, 1 + 1.18, 1 + 1.82, 1 + 2.18, 1 + 2.82, 1 + 3.18,
+                         1 + 3.82, 1 + 4.18, 1 + 4.82, 1 + 5.18, 1 + 5.82, 1 + 6.18), y = 92.5, 
+           label = plabel, cex = 5)+
+  labs(y = 'Estimated Health', x = 'Injury Status', fill = 'Reproductive Status')+
+  scale_x_discrete(labels = c('Unimpacted', 'Minor\nNo Gear', 'Minor\nGear', 'Moderate\nNo Gear','Moderate\nGear',   'Severe\nNo Gear', 'Severe\nGear'))+
+  scale_fill_grey(start = 1, end = 0.65,labels = c('Non-repro\nFemales', 'Repro Females'))+
+  theme_bw(base_size = 18)
+p
 
 
 pdf(file = name, width = 8, height = 6)
