3fb9b51a7ceecb28ecddc8143f85e5553437b6d3
= <=>
2015-12-07 15:24:24 +0000

    Code is in to tally the first three lines of the table: number, max duration, and sum of entangled days

--- R/amyTableTally.R
+++ R/amyTableTally.R
@@ -3,8 +3,23 @@
 
 rm(list=ls())
 library(knitr)
+library(lubridate)
 library(dplyr)
 source('/Users/rob/Documents/code/rss10/rightwhales/makeYearmon.r')
+setwd('/Users/rob/Rdev/tangled')
 load(file = 'data/eg_2015_newData_JUVTRUE__50000_wkspc.rdata')
 load(file="data/egAmyEntData.rdata") # egAmyEntData.rdata contains tangleOut, tangRepro, tangNonRepro, so use the repro flag
 
+# nind in each class
+nind <- tangleOut %>% 
+  group_by(gearInj) %>% 
+  summarise(n = n())
+nind
+
+# months in each class
+nmon <- tangleOut %>% 
+  mutate(elapsed = EndDateWindow - StartDateWindow) %>% 
+  group_by(gearInj) %>% 
+    summarise(maxDur = max(elapsed),
+              sumDays = sum(elapsed))
+nmon
