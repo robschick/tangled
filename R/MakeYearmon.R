6148da82b9c36010ff3c8e2e244d4ad51e5dcc46
= <=>
2016-02-01 16:22:37 +0000

    Adding in two existing helper functions

---
+++ R/MakeYearmon.R
@@ -0,0 +1,8 @@
+# to make a year format from monYr for the right whale plotting
+makeYearmon <- function(timedat){
+	if(is.null(nrow(timedat))){	months <- timedat[1]} else {	months <- timedat[,1]}
+	if(is.null(nrow(timedat))){	years <- timedat[2]} else {years <- timedat[, 2]}
+	dates <- paste(months, '01', years, sep = '/')
+	yearMonth <- as.Date(dates, '%m/%d/%Y')
+	return(yearMonth)
+}
