60a4a40859df98a2daa0bc884b1e0b5876ef6730
= <=>
2016-02-04 14:19:54 +0000

    Check-in of the Anomlay code and data The idea was to make something that I could store in the data folder and then re-use when I want to assess the effects of entanglement with plotting. Here is a function that returns the anomaly, the handler script that calls that function, and the actual rda file that I'm checking in.

---
+++ R/prepAnomaly.R
@@ -0,0 +1,5 @@
+rm(list = ls())
+load(file = 'data/eg_203_ng_50000.rdata')
+source('r/returnhealthAnom.R')
+anom <- returnhealthAnom()
+save(anom, file = 'data/healthAnomaly.rda')
---
+++ R/returnhealthAnom.R
@@ -0,0 +1,36 @@
+#' Return the right whale population level health Anomaly as a data frame
+#' 
+#' @param ngg A scalar corresponding to the number of Gibbs steps
+#' @param tIndex A vector containing values 1:nt
+#' @param monYr An nt by 2 matrix containing months in the first column and years in the second
+#' @param sumh A matrix containing the estimates of right whale health for all whales
+#' @param newgibbsMales A matrix containing the estimates of right whale health for Adult Males
+#' @param newgibbsJuvs1 A matrix containing the estimates of right whale health for Young Juveniles
+#' @param newgibbsJuvs3 A matrix containing the estimates of right whale health for Old Juveniles
+#' 
+#' @return A data frame of right whale population level health anomaly, e.g. observed health (matrix) - pop health (vector)
+#' @example returnhealthAnom()
+returnhealthAnom <- function() {
+  
+  healthmean <- sumh / g
+  healthmeanM <- newhgibbsMales / g
+  healthmeanJ1 <- newhgibbsJuvs1 / g
+  healthmeanJ2 <- newhgibbsJuvs3 / g
+
+  for(i in 1:nrow(healthmean)){
+    
+    healthmean[i, 1:firstSight[i]] <- NA
+    healthmeanM[i, 1:firstSight[i]] <- NA
+    healthmeanJ1[i, 1:firstSight[i]] <- NA
+    healthmeanJ2[i, 1:firstSight[i]] <- NA
+
+  }
+  
+  newPopCat <- rbind(healthmeanM, healthmeanJ1, healthmeanJ2)
+  newPopMedian <- apply(newPopCat, 2, median, na.rm = TRUE)
+  
+  popAnom <- healthmean - newPopMedian
+  
+  popAnom
+}
+
