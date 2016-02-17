50ee05f288f4832e00502fa0dfea88c0b2beecf8
= <=>
2016-02-17 15:25:06 +0000

    Final check-in of the draft - pre-Amy Ok, I've made all of the changes I set out to, and updated it a wee bit by adding in a call to a named vector to get the labels and numbers correct for the enumeration of the entanglement events.

--- R/boxplotHealth.R
+++ R/boxplotHealth.R
@@ -154,22 +154,38 @@ save(dfLongRepro, dfLongNonRepro, file = 'data/healthEntanglementWindowDataBoth.
 
 # set up for labeling the box plots
 dfLong <- rbind(dfLongRepro, dfLongNonRepro)
-nvals <- as.vector(table(dfLong$status, dfLong$gearInj))
-plabel <- c(nvals[11], nvals[12], nvals[9], nvals[10], nvals[7], nvals[8],
-            nvals[5], nvals[6], nvals[3], nvals[4], nvals[1], nvals[2])
+namevec = c('Unimpacted'=0, 'Minor No Gear'=6, 'Minor Gear'=4, 'Moderate No Gear'=5,
+            'Moderate Gear'=2, 'Severe No Gear'=3, 'Severe Gear'=1)
+
+nvals <- as.data.frame(table(dfLong$status, dfLong$gearInj))
+
+nvals$label <- namevec[nvals$Var2]
+
+plabel <- c(nvals$Freq[nvals$Var1 == 'NonRepFem' & nvals$Var2 == 6], 
+            nvals$Freq[nvals$Var1 == 'RepFem' & nvals$Var2 == 6], 
+            nvals$Freq[nvals$Var1 == 'NonRepFem' & nvals$Var2 == 4], 
+            nvals$Freq[nvals$Var1 == 'RepFem' & nvals$Var2 == 4], 
+            nvals$Freq[nvals$Var1 == 'NonRepFem' & nvals$Var2 == 5], 
+            nvals$Freq[nvals$Var1 == 'RepFem' & nvals$Var2 == 5], 
+            nvals$Freq[nvals$Var1 == 'NonRepFem' & nvals$Var2 == 2], 
+            nvals$Freq[nvals$Var1 == 'RepFem' & nvals$Var2 == 2], 
+            nvals$Freq[nvals$Var1 == 'NonRepFem' & nvals$Var2 == 3], 
+            nvals$Freq[nvals$Var1 == 'RepFem' & nvals$Var2 == 2], 
+            nvals$Freq[nvals$Var1 == 'NonRepFem' & nvals$Var2 == 1], 
+            nvals$Freq[nvals$Var1 == 'RepFem' & nvals$Var2 == 1])
 
 name <- paste('/Users/rob/Dropbox/Papers/KnowltonEtAl_Entanglement/images/BoxPlotHealthByEntglClass_NonReproAndReproAnimals_', Sys.Date(), '.pdf', sep = '')
 # This updated plot is per Amy's request to include both categories side by side. Instead of breaking down the gear/no-gear for shading
 # the shading will be repro vs non-repro
 p <- ggplot(dfLong, aes(x = factor(gearInj, levels = c(0, 6, 4, 5, 2, 3, 1)), y = hAnom)) +
-  # geom_boxplot()
-  geom_boxplot(aes(fill = factor(status), group = paste(factor(variable), group)), outlier.shape=NA, notch = FALSE)
-  p
+  geom_hline(aes(yintercept = 0), colour = 'grey50')+
+  geom_boxplot(aes(fill = factor(status), group = paste(factor(gearInj), status)), outlier.shape=NA, notch = FALSE)+
   annotate('text', x = c(1 + 0.82, 1 + 1.18, 1 + 1.82, 1 + 2.18, 1 + 2.82, 1 + 3.18,
                          1 + 3.82, 1 + 4.18, 1 + 4.82, 1 + 5.18, 1 + 5.82, 1 + 6.18), y = 25, 
            label = plabel, cex = 5)+
   labs(y = 'Deviation From Population Health', x = 'Injury Status', fill = 'Reproductive Status')+
   scale_x_discrete(labels = c('Unimpacted', 'Minor\nNo Gear', 'Minor\nGear', 'Moderate\nNo Gear','Moderate\nGear',   'Severe\nNo Gear', 'Severe\nGear'))+
+  
   scale_fill_grey(start = 1, end = 0.65,labels = c('All Other\nDemographic\nCategories\n', 'Reproductive\nFemales\n'))+
   theme_bw(base_size = 16)
 p
