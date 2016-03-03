74ac77900ba5fcc8e1d4fe683a4745f7a92d4d50
= <=>
2016-03-03 16:10:25 +0000

    Adding in the code to extract the death times, and construct the data frame to be used in plotting

--- R/time2death.R
+++ R/time2death.R
@@ -12,6 +12,7 @@ library(gridExtra)
 library(RColorBrewer)
 library(stringr)
 library(plyr)
+library(dplyr)
 source('/Users/rob/Documents/code/rss10/rightwhales/makeYearmon.r')
 wd <- '/Users/rob/Documents/research/projects/PCAD/rightwhales/'
 setwd(wd)
@@ -19,12 +20,23 @@ load(file = paste(wd, 'gibbsoutput/eg_2015_newData_JUVTRUE__50000_wkspc.rdata', 
 load(file="data/egAmyEntData.rdata")
 tSub <- tangleOut
 
-events <- numeric(0)
-for(egno in unique(tSub$EGNo)){
-  dsub <- subset(tSub, EGNo == egno)
-  dsub <- dsub[which.max(dsub$EventNo),]
-  events <- rbind(events, dsub)
-}
+# events <- numeric(0)
+# for(egno in unique(tSub$EGNo)){
+#   dsub <- subset(tSub, EGNo == egno)
+#   dsub <- dsub[which.max(dsub$EventNo),]
+#   events <- rbind(events, dsub)
+# }
+# events <- events[order(events$EGNo),]
+
+# for seom reason, two of these contain duplicated info: 1249 & 1980, so I'm going to remove one of them
+idx <- which(tSub$EGNo == 1249 & tSub$EventNo == 2)
+tSub <- tSub[-idx[2],]
+idx <- which(tSub$EGNo == 1980 & tSub$EventNo == 2)
+tSub <- tSub[-idx[2],]
+
+# find the last event
+events <- tSub %>% group_by(EGNo) %>% top_n(n=1, EventNo) %>% arrange(EGNo)
+
 
 # need to find the max estimate for death from deathyr
 # this will produce 'dtime' which is that month of imputed death
@@ -40,9 +52,33 @@ for(i in 1:length(unique(events$EGNo))){
     events$knownD[i] <- TRUE
   }
 }
-
+events <- mutate(events, 
+                 emonyrID = match(emonyr, myName),
+                 ldwgmonyrID = match(ldwgmonyr, myName))
 # table(events$knownD)
 
+survdf <- numeric(0)
+for(i in 1:nrow(events)){
+  id <- events[i, 'EGNo']
+  dmonth <- as.numeric(events[i, 'dtime'])
+  if(is.na(events[i, 'ldwgmonyrID'])) {emonth <- as.numeric(events[i, 'emonyrID'])
+  } else {
+    emonth <- as.numeric(events[i, 'ldwgmonyrID'])  
+  }
+  
+  kd <- data.frame(events[i, 'knownD'])
+  censor <- ifelse(dmonth > nt, TRUE, FALSE)
+  cmonth <- dmonth
+  dmonth2 <- dmonth 
+  svec <- seq(emonth, dmonth2)
+  svec0 <- svec - min(svec)
+  survdf <- rbind(survdf, data.frame(EGNo = id, deathMonth = dmonth, censored = censor, censMonth = cmonth, survTime0 = svec0, 
+                                     deathMonth0 = max(svec0), severity = events[i, 'Severity'], sevNumClass = events[i ,'gearInj'],
+                                     knownDeath = kd))
+}
+
+survdf$censMonth[survdf$censMonth < nt] <- NA
+save(survdf, ID, gender, file = '/Users/rob/Dropbox/Papers/KnowltonEtAl_Entanglement/data/kmcalcInput.rda')
 
 df <- events # as compared to dfsub, this includes all animals 
 df$dtime <- ifelse(df$dtime > nt, nt, df$dtime)
@@ -63,41 +99,41 @@ dfsub$ldwgmonyrID <- match(dfsub$ldwgmonyr, myName)
 dfsub$m2die <- ifelse(!is.na(dfsub$ldwgmonyrID), dfsub$dtime - dfsub$ldwgmonyrID, dfsub$dtime - dfsub$emonyrID)
 dfsubsum <- data.frame(label = c('Severe - gear', 'Moderate - gear', 'Severe - no gear', 'Minor - gear', 'Moderate - no gear', 'Minor - no gear'),
                        medianMonths = floor(aggregate(dfsub$m2die, list(dfsub$gearInj), median, na.rm = TRUE))[, 2], 
-                    sdMonths = floor(aggregate(dfsub$m2die, list(dfsub$gearInj), sd, na.rm = TRUE))[, 2])
+                       sdMonths = floor(aggregate(dfsub$m2die, list(dfsub$gearInj), sd, na.rm = TRUE))[, 2])
 write.csv(dfsubsum, file = '/Users/rob/Dropbox/Papers/KnowltonEtAl_Entanglement/data/time2Death.csv', row.names = FALSE)
 
 # # Plot out the distributions
 pdf(file = '/Users/rob/Dropbox/Papers/KnowltonEtAl_Entanglement/images/time2die.pdf', width = 9.4, height = 5.9)
-  x <- seq(-4, 4, length=10000)
+x <- seq(-4, 4, length=10000)
 par(xpd = FALSE)
-  plot(x, xlim=c(0, 250), ylim=c(1, 6), xlab='Time until Death (Years)', ylab="", type="n", las = 1, bty = 'n', axes = FALSE)
-  axis(side = 2, at = seq(6), 
+plot(x, xlim=c(0, 250), ylim=c(1, 6), xlab='Time until Death (Years)', ylab="", type="n", las = 1, bty = 'n', axes = FALSE)
+axis(side = 2, at = seq(6), 
      labels = c('Severe\n Gear', 'Moderate\nGear', 'Severe\nNo Gear', 
                 'Minor\nGear', 'Moderate\nNo Gear', 'Minor\nNo Gear'), las = 1, cex.axis = .75)
-  axis(side = 1, at = seq(0, 20 * 12, by = 12), labels = seq(0, 20 * 12, by = 12) / 12, cex.axis = .65) # x axis
-  abline(v = seq(12, 20 * 12, by = 12), col = 'grey80')
-  abline(v = seq(60, 20 * 12, by = 60), col = 'grey40')
-  denstrip(x = dfsub$m2die[dfsub$gearInj == 1], at = 1, ticks = median(dfsub$m2die[dfsub$gearInj == 1]), twd = 3)
-  denstrip(x = dfsub$m2die[dfsub$gearInj == 2], at = 2, ticks = median(dfsub$m2die[dfsub$gearInj == 2]), twd = 3)
-  denstrip(x = dfsub$m2die[dfsub$gearInj == 3], at = 3, ticks = median(dfsub$m2die[dfsub$gearInj == 3]), twd = 3)
-  denstrip(x = dfsub$m2die[dfsub$gearInj == 5], at = 5, ticks = median(dfsub$m2die[dfsub$gearInj == 5]), twd = 3)
-  denstrip(x = dfsub$m2die[dfsub$gearInj == 6], at = 6, ticks = median(dfsub$m2die[dfsub$gearInj == 6]), twd = 3)
+axis(side = 1, at = seq(0, 20 * 12, by = 12), labels = seq(0, 20 * 12, by = 12) / 12, cex.axis = .65) # x axis
+abline(v = seq(12, 20 * 12, by = 12), col = 'grey80')
+abline(v = seq(60, 20 * 12, by = 60), col = 'grey40')
+denstrip(x = dfsub$m2die[dfsub$gearInj == 1], at = 1, ticks = median(dfsub$m2die[dfsub$gearInj == 1]), twd = 3)
+denstrip(x = dfsub$m2die[dfsub$gearInj == 2], at = 2, ticks = median(dfsub$m2die[dfsub$gearInj == 2]), twd = 3)
+denstrip(x = dfsub$m2die[dfsub$gearInj == 3], at = 3, ticks = median(dfsub$m2die[dfsub$gearInj == 3]), twd = 3)
+denstrip(x = dfsub$m2die[dfsub$gearInj == 5], at = 5, ticks = median(dfsub$m2die[dfsub$gearInj == 5]), twd = 3)
+denstrip(x = dfsub$m2die[dfsub$gearInj == 6], at = 6, ticks = median(dfsub$m2die[dfsub$gearInj == 6]), twd = 3)
 dev.off()
 
 pdf(file = '/Users/rob/Dropbox/Papers/KnowltonEtAl_Entanglement/images/time2dieAll.pdf', width = 9.4, height = 5.9)
-  plot(x, xlim=c(0, 250), ylim=c(1, 6), xlab='Time (Years)', ylab="", type="n", las = 1, bty = 'n', axes = FALSE)
-  axis(side = 2, at = seq(6), 
+plot(x, xlim=c(0, 250), ylim=c(1, 6), xlab='Time (Years)', ylab="", type="n", las = 1, bty = 'n', axes = FALSE)
+axis(side = 2, at = seq(6), 
      labels = c('Severe\n Gear', 'Moderate\nGear', 'Severe\nNo Gear', 
                 'Minor\nGear', 'Moderate\nNo Gear', 'Minor\nNo Gear'), las = 1, cex.axis = .75)
-  axis(side = 1, at = seq(0, 20 * 12, by = 12), labels = seq(0, 20 * 12, by = 12) / 12, cex.axis = .65) # x axis
-  abline(v = seq(12, 20 * 12, by = 12), col = 'grey80')
-  abline(v = seq(60, 20 * 12, by = 60), col = 'grey40')
-  denstrip(x = df$m2die[df$gearInj == 1], at = 1, ticks = median(df$m2die[df$gearInj == 1]), twd = 3)
-  denstrip(x = df$m2die[df$gearInj == 2], at = 2, ticks = median(df$m2die[df$gearInj == 2]), twd = 3)
-  denstrip(x = df$m2die[df$gearInj == 3], at = 3, ticks = median(df$m2die[df$gearInj == 3]), twd = 3)
-  denstrip(x = df$m2die[df$gearInj == 3], at = 4, ticks = median(df$m2die[df$gearInj == 4]), twd = 3)
-  denstrip(x = df$m2die[df$gearInj == 5], at = 5, ticks = median(df$m2die[df$gearInj == 5]), twd = 3)
-  denstrip(x = df$m2die[df$gearInj == 6], at = 6, ticks = median(df$m2die[df$gearInj == 6]), twd = 3)
+axis(side = 1, at = seq(0, 20 * 12, by = 12), labels = seq(0, 20 * 12, by = 12) / 12, cex.axis = .65) # x axis
+abline(v = seq(12, 20 * 12, by = 12), col = 'grey80')
+abline(v = seq(60, 20 * 12, by = 60), col = 'grey40')
+denstrip(x = df$m2die[df$gearInj == 1], at = 1, ticks = median(df$m2die[df$gearInj == 1]), twd = 3)
+denstrip(x = df$m2die[df$gearInj == 2], at = 2, ticks = median(df$m2die[df$gearInj == 2]), twd = 3)
+denstrip(x = df$m2die[df$gearInj == 3], at = 3, ticks = median(df$m2die[df$gearInj == 3]), twd = 3)
+denstrip(x = df$m2die[df$gearInj == 3], at = 4, ticks = median(df$m2die[df$gearInj == 4]), twd = 3)
+denstrip(x = df$m2die[df$gearInj == 5], at = 5, ticks = median(df$m2die[df$gearInj == 5]), twd = 3)
+denstrip(x = df$m2die[df$gearInj == 6], at = 6, ticks = median(df$m2die[df$gearInj == 6]), twd = 3)
 dev.off()
 
 # summary statistics
