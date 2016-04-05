ff5085664ef58e34a2b96fac8cb470b6664325c6
= <=>
2016-04-05 17:03:41 +0100

    Update documentation of data files
    One is for things that are prepared from external data, like the entanglement time data from Amy.
    
    The second function - helperData.R - summarises health, and health anomaly data that come from the mdoel output.

---
+++ R/helperData.R
@@ -0,0 +1,45 @@
+#' Health anomaly between individual health and population median health
+#' 
+#' This is a matrix of differences between individual health 
+#' and population level health. I calculate this anomaly by first 
+#' calculating the median health of the target groups (Adult males,
+#' and young and old juveniles), and then subtracting that value
+#' from the median health of each individual.
+#' 
+#' @format A matrix of dimensions 696 by 564; each row contains the estimates
+#' of health anomaly for an individual. Each column is a month.
+'anom'
+
+#' Estimated health of individual animals
+#' 
+#' This is a matrix that of individual health estimated by the model
+#' in Schick et al. 2013 PLoS-ONE.
+#' 
+#' @format A matrix of dimensions 696 by 564; each row contains the estimates
+#' of health for an individual. Each column is a month.
+'healthmean'
+
+#' ID vector of EGNo's for all individual right whales in the data base
+#' 
+#' This vector is generated and curated by the New England Aquarium. 
+#' Each value in this vector is a unique and unchanging identifier 
+#' of individual right whales
+#' 
+#' @format A vector of length 696
+'ID'
+
+#' Length of the modelling
+#' 
+#' This is simply calculated with \code{length(myName)} and shows the 
+#' temporal extent of the modelling.
+#' 
+#' @format A vector of length 1
+'nt'
+
+#' Month-year vector used to mark time in the model
+#' 
+#' This starts at January 1970, and goes through September, 2016,
+#' or roughly the temporal extent of modelling. Values are like '1-1970'
+#' 
+#' @format A vector of length 696
+'myName'
