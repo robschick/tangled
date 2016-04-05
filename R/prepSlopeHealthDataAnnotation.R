8595a798b7d105fe963433356d9cd1cbc87e8c22
= <=>
2016-04-05 16:51:42 +0100

    Make old pop health script into 5 functions
    1. plotSlopeHealth.R - to make the graph
    2. prepSlopeHealthData.R - to make dfout to plot the individual lines
    3. prepSlopeHealthDataMedian.R - to plot the summary lines in each of the 6 facets
    4. prepSlopeHealthDataAnnotation.R - to make the annotations for the 6 facets, i.e. the number of events per category
    5. prepSlopeHealthDataBarplot.R - to prepare the data to make the bar plots in the lower half of this plot

---
+++ R/prepSlopeHealthDataAnnotation.R
@@ -0,0 +1,17 @@
+#' Prepare the data for annotating the slope health plot
+#'
+#' This function starts with the \code{dfout} data frame, and puts that
+#' through a \code{dplyr} wringer to calculate the number of cases
+#' in each of the 6 different entanglement types.
+#' 
+#' @return \code{dfn} A data frame containing the tally of the number of
+#' events in each entanglement type.
+#' @example prepSlopeHealthDataAnnotation(dfout)
+prepSlopeHealthDataAnnotation <- function(dfout){
+  
+  dfn <- dfout %>% 
+    group_by(gearInj) %>% 
+    summarise(n = n_distinct(egno))
+  dfn
+
+}
---
+++ R/prepSlopeHealthDataMedian.R
@@ -0,0 +1,24 @@
+#' Prepare the median line data for slope health plot
+#'
+#' This function starts with \code{dfout} and calculates median summary
+#' lines to be used in the plotting of the upper panel of the slope plot
+#' 
+#' @return \code{dfoutMed} A data frame containing the median health and 
+#' health anomaly for each entanglement event at each of the three 
+#' locations
+#' @example prepSlopeHealthDataMedian(dfout)
+prepSlopeHealthDataMedian <- function(dfout){
+  
+  dfAsum <- dfout %>% 
+    group_by(gearInj) %>% 
+    summarise(sAnom = median(startAnom, na.rm = TRUE), 
+              endAnom = median(endAnom), 
+              recAnom = median(recAnom),
+              fullLab = unique(fullLab),
+              sevLab = unique(sevLab),
+              gearLab = unique(gearLab))
+  dfoutMed <- reshape2::melt(dfAsum, id.vars = c('gearInj', 'fullLab', 'sevLab', 'gearLab'))
+  dfoutMed$x <- rep(0:2, each = 6)
+  dfoutMed
+ 
+}
