173e7e528d1960b89a5ca158a8972e88a20ad383
= <=>
2016-04-05 16:54:51 +0100

    Add necessary helper variables for subsequent plotting

--- data-raw/prepAnomaly.R
+++ data-raw/prepAnomaly.R
@@ -4,3 +4,12 @@ source('r/returnhealthAnom.R')
 anom <- returnhealthAnom()
 # save(anom, file = 'data/healthAnomaly.rda')
 devtools::use_data(anom)
+
+healthmean <- sumh / g
+devtools::use_data(healthmean)
+
+devtools::use_data(ID)
+
+devtools::use_data(nt)
+
+devtools::use_data(myName)
