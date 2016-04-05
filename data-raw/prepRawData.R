66df0bb3f902ce27ee9eefdc774add7f319463fa
= <=>
2016-04-05 12:45:12 +0100

    removing the data prep to make the excel files Amy had sent me as objects.

--- data-raw/prepRawData.R
+++
@@ -1,5 +0,0 @@
-etime <- read_csv(file = '../inst/extdata/TimingEntanglementReformatDate.csv')
-devtools::use_data(etime, overwrite = TRUE)
-
-estStartDates <- read_csv(file = '../inst/extdata/EntglEstimatedStartDates.csv')
-devtools::use_data(estStartDates, overwrite = TRUE)
