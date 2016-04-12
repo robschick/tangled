cca683e643b1a1fcde552174991e09fc4d2cb6a8
= <=>
2016-04-12 11:03:24 +0100

    Prepare functions to make data for plotting survivorship
    Goal of these is to take what had been one script and turn it into two separate functions: 1) to prepare the data for plotting the KM curves for all animals; and 2) to prepare the data for plotting the KM curves split out by gender and by entanglement severity.

--- R/calcKMCurves.R
+++ R/calcKMCurves.R
@@ -1,112 +1,64 @@
-# rm(list = ls())
-library(dplyr)
-
-
-load(file = 'data/kmcalcInput.rda') # canonical data for making the median curves; 
-# contains: survdf, kdpasurvldf, ID, gender, dcut, myName
-canonsurvdf <- survdf
-load(file="data/events.rda") # use this to call the data presDeadtime.R
-load(file = 'data/deathyr.rda') # contains deathyr
-# library(reportCard)
-# deathyr <- deathyr203
-nt <- dcut
-
-nboot <- 1
-kmlines <- vector(mode = 'list', (nboot + 1)) # idea of the dimension is to put the median value in the first list slot
-censTicks <- vector(mode = 'list', (nboot + 1))
-increment <- 12 # these are the number of months to increment over; 12 == 1 year - duh.
-for(nb in 1:(nboot + 1)) {
+#' Return lists to be used in plotting Kaplan-Meier survival curves
+#' 
+#' This function takes the output from the functions used to make
+#' the survival data for the known dead, presumed alive, and presumed
+#' dead animals, and prepare the survivorship information needed to 
+#' plot the KM curves. If needed, we can vary the interval over which
+#' the survivorship is calculated. And we can store uncertainty
+#' around the death times through the use of a boot strap
+#' 
+#' @usage calcKMCurves(survdf, kdpasurvldf, nboot = 1, dcut, increment = 12)
+#' @param \code{survdf} is the data frame containing survival data for all
+#'     animals in the three possible categories. 
+#' @param \code{kdpasurvldf} is the data frame containing survival data for
+#'     just the known dead and presumed alive animals
+#' @param \code{nboot} an integer indicating the number of times we want to
+#'     run a bootstrap to collect uncertainty around the survivorship. Default
+#'     is to not run a boot strap
+#' @param \code{dcut} integer indicating the month-year combo when the sightings 
+#'     data are considered complete through
+#' @param \code{increment} the unit of time overwhich we calculate survivorship
+#'     Default value is to calculate it over a yearly interval
+#' @example calcKMCurves(survdf, kdpasurvldf, nboot = 1, dcut, increment = 12)
+#' @return A list with two elements: 1) \code{kmlines} and 2) \code{censTicks}.
+#'     The first element is a data frame of the survivorship for the different
+#'     time frames. The second element is a data frame of censor times - simply
+#'     used to make tick marks of when the animal was removed from the study
+#'     via censoring.
+calcKMCurves <- function(survdf, kdpasurvldf, nboot = 1, dcut, increment = 12){
   
-  if (nb == 1) {
-    survdf <- canonsurvdf
-  } else {
-    deathSamp <- getDeaths(deathyr)
-    enew <- presDeadsurvdat(events, dcut, deathSamp)
-    survdf <- rbind(enew, kdpasurvldf)  
-  }
+  medsurvdf <- kdpasurvldf
+  nt <- dcut
   
-  survdf$yearInt <- findInterval(survdf$survTime0, seq(0, nt, by = increment)) # to group the data into yearly summaries
-  survdf$censYearInt <- findInterval(survdf$censMonth0, seq(0, nt, by = increment)) # to group the death data into yearly 
-  survdf$deathyearInt <- findInterval(survdf$deathMonth0, seq(0, nt, by = increment)) # to group the death data into yearly 
-  dsub <- survdf
+  # set up lists to hold output; if we run a bootstrap each
+  # output from the bootstrap gets placed into a list element:
+  kmlines <- vector(mode = 'list', (nboot)) 
+  censTicks <- vector(mode = 'list', (nboot))
   
-  kmdf <- data.frame(interval = 0, atRiskStart = nrow(dsub), censored = 0,
-                     diedOnInterval = 0, atRiskEnd = 0, propSurv = 1) 
-  
-  for(i in 1:length(unique(dsub$yearInt))){
-    dsubi <- dsub[which(dsub$yearInt == i), ]
-    interval <- i
+  for(nb in 1:(nboot)) {
     
-    atRiskStart <- length(unique(dsubi$EGNo))
-    censored <- length(which(!duplicated(dsubi$EGNo) & dsubi$censored == TRUE & dsubi$deathyearInt == interval))
-    died <- length(which(!duplicated(dsubi$EGNo) & dsubi$censored == FALSE & dsubi$deathyearInt == interval))
-    atRiskEnd <- atRiskStart - (censored + died)
-    proportionSurviving <- atRiskEnd / atRiskStart
+    # The default is to _not_ run a bootstrap, which means
+    # the survdf input is what we'll use. If we do run 
+    # a boot strap, then we'll bind together the newly
+    # sampled data along with the median survival data frame
+    # which is input to the function:
+    if (nb > 1) {
+      deathSamp <- getDeaths(deathyr)
+      enew <- presDeadsurvdat(events, dcut, deathSamp)
+      survdf <- rbind(enew, medsurvdf)  
+    }
     
-    kmdf <- rbind(kmdf, data.frame(interval = interval, atRiskStart = atRiskStart, censored = censored,
-                                   diedOnInterval = died, atRiskEnd = atRiskEnd, propSurv = proportionSurviving))
-  }
-  kmdf$psurv <- cumprod(kmdf$propSurv)
-  kmdf$group <- paste('iter', nb, sep = '')
-  kmlines[[nb]] <- kmdf
-  
-  # I want a censored month in here as well for plotting purposes, i.e. to make the ticks
-  csub <- subset(dsub, censored == TRUE)
-  csub <- csub[!duplicated(csub$EGNo),]
-  psvec <- rep(NA, nrow(csub))# to store the death months
-  
-  for(i in 1:nrow(csub)){
-    interval <- csub[i, 'censYearInt']
-    psvec[i] <- kmdf[kmdf$interval == (interval - 1), 'psurv']
-  }
-  
-  csub$psurv <- psvec
-  csub$group <- paste('iter', nb, sep = '')
-  censTicks[[nb]] <- csub
-} # end bootstrap interval
-
-
-
-
-
-
-
-# Break it out by entanglement severity & Gender:
-# Get the gender in using ID and Gender
-t1 <- Sys.time()
-nboot <- 1
-increment <- 12 # these are the number of months to increment over; 12 == 1 year - duh.
-kmlines <- vector(mode = 'list', (nboot + 1)) # idea of the dimension is to put the median value in the first list slot
-censTicks <- vector(mode = 'list', (nboot + 1))
-
-for(nb in 1:(nboot + 1)) {
-  
-  if (nb == 1) {
-    survdf <- canonsurvdf
-  } else {
-    deathSamp <- getDeaths(deathyr)
-    enew <- presDeadsurvdat(events, dcut, deathSamp)
-    survdf <- rbind(enew, kdpasurvldf)  
-  }
-  
-survdf$gender <- gender[match(survdf$EGNo, ID)]
-survdf$yearInt <- findInterval(survdf$survTime0, seq(0, nt, by = increment)) # to group the data into yearly summaries
-survdf$censYearInt <- findInterval(survdf$censMonth0, seq(0, nt, by = increment)) # to group the death data into yearly 
-survdf$deathyearInt <- findInterval(survdf$deathMonth0, seq(0, nt, by = increment)) # to group the death data into yearly 
-svvec <- c("moderate", "minor", "severe" )
-kmdfAll <- numeric(0) # is this the right place for this?
-csubAll <- numeric(0)
-
-for(g in c("M", "F")) {
-  
-  for(j in 1:3){
+    # to group the data into temporal units, e.g. yearly
+    survdf$yearInt <- findInterval(survdf$survTime0, seq(0, nt, by = increment)) 
+    survdf$censYearInt <- findInterval(survdf$censMonth0, seq(0, nt, by = increment)) 
+    survdf$deathyearInt <- findInterval(survdf$deathMonth0, seq(0, nt, by = increment))
+    dsub <- survdf
     
-
-    dsub <- survdf[survdf$Severity == svvec[j] & survdf$gender == g, ]
     kmdf <- data.frame(interval = 0, atRiskStart = nrow(dsub), censored = 0,
-                       diedOnInterval = 0, atRiskEnd = 0, propSurv = 1, gender = g)
+                       diedOnInterval = 0, atRiskEnd = 0, propSurv = 1) 
     
-    for(i in 1:length(unique(dsub$yearInt))){ 
+    for(i in 1:length(unique(dsub$yearInt))){
+      
       dsubi <- dsub[which(dsub$yearInt == i), ]
       interval <- i
       
@@ -116,36 +68,33 @@ for(g in c("M", "F")) {
       atRiskEnd <- atRiskStart - (censored + died)
       proportionSurviving <- atRiskEnd / atRiskStart
       
-      kmdf <- rbind(kmdf, data.frame(interval = interval, atRiskStart = atRiskStart, censored = censored,
-                                     diedOnInterval = died, atRiskEnd = atRiskEnd, propSurv = proportionSurviving, gender = g))
+      kmdf <- rbind(kmdf, data.frame(interval = interval, atRiskStart = atRiskStart, 
+                                     censored = censored, diedOnInterval = died, 
+                                     atRiskEnd = atRiskEnd, propSurv = proportionSurviving))
     }
     
     kmdf$psurv <- cumprod(kmdf$propSurv)
-    kmdf$sev <- svvec[j]
-    kmdfAll <- rbind(kmdfAll, kmdf)
-    kmlines[[nb]] <- data.frame(kmdfAll, group = paste('iter', nb, sep = ''))
+    kmdf$group <- paste('iter', nb, sep = '')
+    kmlines[[nb]] <- kmdf
     
-    # I want a censored month in here as well for plotting purposes
+    # I want a censored month in here as well for plotting purposes, i.e. to make the ticks
     csub <- subset(dsub, censored == TRUE)
     csub <- csub[!duplicated(csub$EGNo),]
-    psvec <- rep(NA, nrow(csub))# to store the death months
+    
+    psvec <- rep(NA, nrow(csub)) # to store the death months
     
     for(i in 1:nrow(csub)){
+      
       interval <- csub[i, 'censYearInt']
       psvec[i] <- kmdf[kmdf$interval == (interval - 1), 'psurv']
+    
     }
     
     csub$psurv <- psvec
-    csub$sev <- svvec[j]
+    csub$group <- paste('iter', nb, sep = '')
+    censTicks[[nb]] <- csub
     
-    csubAll <- rbind(csubAll, csub)
-    csubAll$gender <- gender[match(csubAll$EGNo, ID)]
-    censTicks[[nb]] <- data.frame(csubAll, group = paste('iter', nb, sep = ''))
-    
-  } # end loop over severity
-
-  } # gender loop
-
-} # end loop over nboot
-
-Sys.time() - t1
+  } # end bootstrap interval
+  
+  list(kmlines = kmlines, censTicks = censTicks)
+}
